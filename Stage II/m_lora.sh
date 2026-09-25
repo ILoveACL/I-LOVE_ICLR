d18a71f04876236bad0e5a5fbabd238171825298
ILoveACL <107552301358@stu.xju.edu.cn>
2026-09-25 16:25:07 +0800

    Update m_lora.sh

--- Stage II/m_lora.sh
+++ Stage II/m_lora.sh
@@ -1,9 +1,9 @@
-export CUDA_VISIBLE_DEVICES="6,7"
-
-model_name=scorere_Qwen3-8B
-step_num=600
-lora_model=/home/nian/ft_LLM/ft_model/TrainMethod_re_mohler/llm_score_re/save_models/${model_name}/model.pt.step${step_num}
-output_dir=/home/nian/ft_LLM/ft_model/TrainMethod_re_mohler/llm_score_re/save_models/${model_name}/step_${step_num}
+export CUDA_VISIBLE_DEVICES="0,1"
+#Replace this with the appropriate path as needed.
+model_name=
+step_num=
+lora_model=//${model_name}/model.pt.step${step_num}
+output_dir=//${model_name}/step_${step_num}
 python merge_lora.py  \
     --lora_model  $lora_model \
     --output_dir  $output_dir
