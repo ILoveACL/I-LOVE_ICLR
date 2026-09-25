6ec28b2833094e15e9f696d36e015d27c7ae0e6e
ILoveACL <107552301358@stu.xju.edu.cn>
2026-09-25 16:09:04 +0800

    Update inference.py

--- Stage II/inference.py
+++ Stage II/inference.py
@@ -9,12 +9,6 @@ os.environ["NCCL_IB_DISABLE"] = "1"
 # os.environ["VLLM_LOG_LEVEL"] = "ERROR"
 # os.environ["VLLM_LOGGING_LEVEL"] = "ERROR"
 
-standard = f"""评分标准：
-三档（7—8分）：能就所有问题有效作答；能使用一个或多个完整的句子进行表达，所用语句在形式和内容上达到了较高水平；回答内容切题，表达清楚；回答中可以偶有停延、重复和语言错误。
-二档（4—6分）：能用结构简单的句子回答听到的所有问题，且内容基本切题，可以有多处停延、重复和语法错误，但基本能达意；或者只回答了部分问题，但内容切题，表达清楚，停延、重复和语言错误较少。
-一档（1—3分）：能勉强回答部分问题；不能说出完整的句子，表述以零散的词语为主，停延、重复较多，但说出的内容与听到的问题有关联。
-0档（0分）：不能作答；或答非所问；或作答的内容完全不可理解。
-"""
 
 def argument():
     parser = argparse.ArgumentParser()
@@ -68,18 +62,9 @@ def read_file(file_path):
             datasets.append(data)
     return datasets
 
-def get_example(examples):
-    example_list = examples.get("example", None)
-
-    if example_list is None:
-        return None
-    else:
-        example_str = "评分示例:\n"
-        for i, k in enumerate(example_list):
-            example_str += f"  示例{i+1}:\n    回答：{k[0]}\n    最终得分：{k[1]}\n"
-    return example_str
 
 
+#We provide a prompt template for the Mohler dataset. For other datasets, simply replace the corresponding sections.
 def apply_prompt(example):
     question = example["question"]
     answer = example["answer"]
@@ -91,9 +76,7 @@ def apply_prompt(example):
     # prompt += standard
 
     prompt += f"question：{question}\n"
-    # ex_str = self.get_example(example)
-    # if ex_str is not None:
-    #     prompt += ex_str
+
     prompt += f"Pending rating response：{answer}\nDirectly provide the final score. The output format is: \nFinal score: Score<|im_end|><|im_start|>assistant\nFinal score: "
     return prompt
 
