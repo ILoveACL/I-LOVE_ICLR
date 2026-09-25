2a883550ab0c4a70ddcb0a406dd96d160b923873
ILoveACL <107552301358@stu.xju.edu.cn>
2026-09-25 16:24:31 +0800

    Update finetune.sh

--- Stage II/finetune.sh
+++ Stage II/finetune.sh
@@ -2,16 +2,16 @@
 export NCCL_P2P_DISABLE=1
 export NCCL_IB_DISABLE=1
 export CUDA_VISIBLE_DEVICES="6,7"
+#Replace this with the appropriate path as needed.
+config_file=/
 
-config_file=/home/nian/ft_LLM/ft_model/TrainMethod_re_mohler/llm_score_re/config.yaml
-
-pre_model=/home/nian/pred_model/LLM_models/Qwen3-8B
-save_dir=/home/nian/ft_LLM/ft_model/TrainMethod_re_mohler/llm_score_re/save_models/scorere_Qwen3-8B
+pre_model=/
+save_dir=/
 
 mkdir -p $save_dir
-data_dir=/home/nian/ft_LLM/datasets/new_local/QAS/train
-train_data=$data_dir/mohler_train.jsonl
-test_data=$data_dir/mohler_eval.jsonl
+data_dir=/
+train_data=$data_dir/train.jsonl
+test_data=$data_dir/eval.jsonl
 log_file=${save_dir}/train.log
 
 accelerate launch --main_process_port 29501 --config_file ${config_file} train.py \
